bfceb8da8cf59ac83cbabf137824940d70a9308b
Hriyen Tripura <hr.daniel777@gmail.com>
2025-04-26 00:55:41 +0530

    backend updated

--- provider.tf
+++ provider.tf
@@ -1,14 +1,15 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.23.0"
     }
   }
+
+ backend "azurerm" {}
 }
 
 provider "azurerm" {
   features {}
-  #subscription_id = "" #"enter your subscription id if required." 
+  # subscription_id = "" # optional if set in service connection
 }
-
